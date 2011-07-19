1e6d04eacd4e469737091817c8ee95c24b546c75
Nicholas Rowe <nixterrimus@gmail.com>
2011-07-19 01:52:33 -0700

    Cleaning up

--- lib/pixel_curtain.rb
+++ lib/pixel_curtain.rb
@@ -2,5 +2,3 @@ dir = File.expand_path(File.dirname(__FILE__))
 require dir + '/pixel_curtain/curtain'
 require dir + '/pixel_curtain/simple_curtain'
 require dir + '/pixel_curtain/complex_curtain'
-
-PixelCurtain::SimpleCurtain
